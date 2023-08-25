e377a95a2d3fa84aa2c146730f9fe513de9180c0
Owen Funk <owentfunk@icloud.com>
2023-08-25 13:04:52 -0400

    commit

---
+++ javaClassesAndObjects.java
@@ -0,0 +1,82 @@
+public class Pet {
+    private String name;
+    private int age;
+    private String location;
+    private String type;
+
+    public Pet() {
+    }
+
+    public Pet(String name, int age, String location, String type) {
+        this.name = name;
+        this.age = age;
+        this.location = location;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public class Car {
+        private String make;
+        private String model;
+        private int year;
+
+        public Car() {
+        }
+
+        public Car(String make, String model) {
+            this.make = make;
+            this.model = model;
+        }
+
+        public Car(String make, String model, int year) {
+            this.make = make;
+            this.model = model;
+            this.year = year;
+        }
+
+        public String getMake() {
+            return make;
+        }
+
+        public String getModel() {
+            return model;
+        }
+
+        public int getYear() {
+            return year;
+        }
+
+        public void setMake(String make) {
+            this.make = make;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        public void setYear(int year) {
+            this.year = year;
+        }
